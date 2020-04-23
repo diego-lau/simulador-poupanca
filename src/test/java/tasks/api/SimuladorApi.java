1ac231ba64d79a7d7277a8e2e23078e296d8cf97
Diego Lau Balz <rhuan.dornelles@dbserver.com.br>
2020-04-23 08:16:59 -0300

    Incluído teste de API mais atualizações

---
+++ src/test/java/tasks/api/SimuladorApi.java
@@ -0,0 +1,22 @@
+package tasks.api;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class SimuladorApi {
+
+    @Test
+    public void getPageOneTest() {
+        String uriBase = "http://5b847b30db24a100142dce1b.mockapi.io/api/v1/simulador";
+        given()
+                .when()
+                .get(uriBase)
+                .then()
+                .statusCode(200) // O status http retornado foi 200
+                .contentType(ContentType.JSON) // O response foi retornado no formato JSON
+                .body("id", is(1)); //A chave "id" contém o valor "1"
+    }
+}
